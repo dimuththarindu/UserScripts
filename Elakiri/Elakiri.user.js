f7eeded3506d9bcc7f61ff28d8434a4ee72d2bbe
DT <25923940+dimuththarindu@users.noreply.github.com>
2019-09-02 04:00:06 +0530

    2.0

--- Elakiri/Elakiri.user.js
+++ Elakiri/Elakiri.user.js
@@ -42,6 +42,30 @@ catch(err) {
 	console.log("Error: " + err);
 }
 
+function funNewDesign() {
+    var css = "";
+
+    css += ".page, table, tbody, .bginput, select, td, .header_box, .postbit_box, .panel, .button, .header_box_guest, center{background: black !important;border-color: #4B515D !important}";
+
+    css += ".thead{background: #2E2E2E !important}";
+
+    css += ".alt1, .alt2{background: #212121 !important}";
+
+    css += ".navbar, .vbmenu_control, .tfoot, .tcat{background: #01579b !important}";
+
+    css += ".vBulletin_editor, .vBulletin_editor td{background: #e0e0e0 !important}";
+
+    css += "strong, a, .alt1, .alt1 div, .alt2, .time, .smallfont, select, .postbit_box, label, .button{color: #F5F5F5 !important}";
+
+    var style = document.createElement("style");
+    style.type = "text/css";
+    style.appendChild(document.createTextNode(css));
+    document.head.appendChild(style);
+
+    var element = document.evaluate('/html/body/table/tbody/tr/td/table[1]/tbody/tr[1]/td/a/img', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    element.src = "https://raw.githubusercontent.com/dimuththarindu/UserScripts/master/Elakiri/Logo.png";
+}
+
 function funRemoveAllElements() {
 	// Downloads
 	funRemoveElement('/html/body/table/tbody/tr/td/div/div/div/table[2]/tbody/tr/td[2]/div[4]');
@@ -81,30 +105,6 @@ function funRemoveAllElements() {
 
 	// Page generated
 	funRemoveElement('/html/body/center');
-}
-
-function funNewDesign() {
-    var css = "";
-
-    css += ".page, table, tbody, .bginput, select, td, .header_box, .postbit_box, .panel, .button, .header_box_guest, center{background: black !important;border-color: #4B515D !important}";
-
-    css += ".thead{background: #2E2E2E !important}";
-
-    css += ".alt1, .alt2{background: #212121 !important}";
-
-    css += ".navbar, .vbmenu_control, .tfoot, .tcat{background: #01579b !important}";
-
-    css += ".vBulletin_editor, .vBulletin_editor td{background: #e0e0e0 !important}";
-
-    css += "strong, a, .alt1, .alt1 div, .alt2, .time, .smallfont, select, .postbit_box, label, .button{color: #F5F5F5 !important}";
-
-    var style = document.createElement("style");
-    style.type = "text/css";
-    style.appendChild(document.createTextNode(css));
-    document.head.appendChild(style);
-
-    var element = document.evaluate('/html/body/table/tbody/tr/td/table[1]/tbody/tr[1]/td/a/img', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-    element.src = "https://raw.githubusercontent.com/dimuththarindu/UserScripts/master/Elakiri/Logo.png";
 }
 
 function funRemoveElement(pathValue) {
