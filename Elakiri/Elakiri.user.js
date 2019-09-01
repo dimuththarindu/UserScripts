5bbd75f448dae6def04a9cb41149ee4486439d99
DT <25923940+dimuththarindu@users.noreply.github.com>
2019-09-02 02:24:04 +0530

    1.2

--- Elakiri/Elakiri.user.js
+++ Elakiri/Elakiri.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Elakiri
 // @namespace    UserScripts
-// @version      1.0
+// @version      1.2
 // @author       DT
 // @description  Clean Elakiri
 // @source       https://github.com/dimuththarindu/UserScripts
@@ -21,55 +21,70 @@
 
 (function() {
     'use strict';
-	
-	// New Design
-	funNewDesign();
-	
-	// Downloads
-	funRemoveElement('/html/body/table/tbody/tr/td/div/div/div/table[2]/tbody/tr/td[2]/div[4]');
-	
-	// ElaKiri Exclusive!
-	funRemoveElement('/html/body/table/tbody/tr/td/div/div/div/table[2]/tbody/tr/td[2]/div[1]');
-	
-	// Checkout!
-	funRemoveElement('/html/body/table/tbody/tr/td/div/div/div/table[2]/tbody/tr/td[3]/div[2]');
-	
-	// Current Poll
-	funRemoveElement('/html/body/table/tbody/tr/td/div/div/div/table[2]/tbody/tr/td[1]/form[2]');
-	
-	// ElaKiri Events!
-	funRemoveElement('/html/body/table/tbody/tr/td/div/div/div/table[2]/tbody/tr/td[1]/div[4]');
-	
-	// Site Navigation
-	funRemoveElement('/html/body/table/tbody/tr/td/div/div/div/table[2]/tbody/tr/td[1]/div[1]');
-	
-	// Navbar Downloads
-	funRemoveElement('/html/body/table/tbody/tr/td/table[1]/tbody/tr[2]/td/table/tbody/tr/td[2]');
-	
-	// Navbar FAQ
-	funRemoveElement('/html/body/table/tbody/tr/td/table[1]/tbody/tr[2]/td/table/tbody/tr/td[2]');
-	
-	// Navbar Contact Us
-	funRemoveElement('/html/body/table/tbody/tr/td/table[1]/tbody/tr[2]/td/table/tbody/tr/td[8]');
-	
-	// Copyright bar
-	funRemoveElement('/html/body/table/tbody/tr/td/div/div/div/div[9]');
-	
-	// Page generated 
-	funRemoveElement('/html/body/center');
+
+    // New Design
+    funNewDesign();
+
+    // Downloads
+    funRemoveElement('/html/body/table/tbody/tr/td/div/div/div/table[2]/tbody/tr/td[2]/div[4]');
+
+    // ElaKiri Exclusive!
+    funRemoveElement('/html/body/table/tbody/tr/td/div/div/div/table[2]/tbody/tr/td[2]/div[1]');
+
+    // Checkout!
+    funRemoveElement('/html/body/table/tbody/tr/td/div/div/div/table[2]/tbody/tr/td[3]/div[2]');
+
+    // Current Poll
+    funRemoveElement('/html/body/table/tbody/tr/td/div/div/div/table[2]/tbody/tr/td[1]/form[2]');
+
+    // ElaKiri Events!
+    funRemoveElement('/html/body/table/tbody/tr/td/div/div/div/table[2]/tbody/tr/td[1]/div[4]');
+
+    // Site Navigation
+    funRemoveElement('/html/body/table/tbody/tr/td/div/div/div/table[2]/tbody/tr/td[1]/div[1]');
+
+    // Navbar Downloads
+    funRemoveElement('/html/body/table/tbody/tr/td/table[1]/tbody/tr[2]/td/table/tbody/tr/td[2]');
+
+    // Navbar FAQ
+    funRemoveElement('/html/body/table/tbody/tr/td/table[1]/tbody/tr[2]/td/table/tbody/tr/td[2]');
+
+    // Navbar Contact Us
+    funRemoveElement('/html/body/table/tbody/tr/td/table[1]/tbody/tr[2]/td/table/tbody/tr/td[8]');
+
+    // Copyright bar
+    funRemoveElement('/html/body/table/tbody/tr/td/div/div/div/div[9]');
+
+    // Page generated 
+    funRemoveElement('/html/body/center');
+
+    // Posting Rules / Sub Thread
+    funRemoveElement('/html/body/table/tbody/tr/td/div[3]/div/div/table[5]');
 })();
 
 function funNewDesign() {
-    var css = ".page, table, tbody, .bginput, select, td, .header_box, .postbit_box, .panel, .button, center { background: black !important; border-color: #4B515D !important;} .thead { background: #2E2E2E !important;} .alt1, .alt2 { background: #212121 !important;} .navbar, .vbmenu_control, .tfoot, .tcat { background: #01579b !important;} .vBulletin_editor, .vBulletin_editor td { background: #e0e0e0 !important; } strong, a, .alt1, .alt2, .time, .smallfont, select, .postbit_box, label, .button { color: #F5F5F5 !important; }";
-	
-	var style = document.createElement("style");
-	style.type = "text/css";
-	style.appendChild(document.createTextNode(css));
-	document.head.appendChild(style);
+    var css = "";
 
-	var element = document.evaluate('/html/body/table/tbody/tr/td/table[1]/tbody/tr[1]/td/a/img', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-	element.id = 'ElakiriInjectLogo';
-	document.getElementById("ElakiriInjectLogo").src = "https://raw.githubusercontent.com/dimuththarindu/UserScripts/master/Elakiri/Logo.png"; 
+    css += ".page, table, tbody, .bginput, select, td, .header_box, .postbit_box, .panel, .button, center{background: black !important;border-color: #4B515D !important}";
+
+    css += ".thead{background: #2E2E2E !important}";
+
+    css += ".alt1, .alt2{background: #212121 !important}";
+
+    css += ".navbar, .vbmenu_control, .tfoot, .tcat{background: #01579b !important}";
+
+    css += ".vBulletin_editor, .vBulletin_editor td{background: #e0e0e0 !important}";
+
+    css += "strong, a, .alt1, .alt1 div, .alt2, .time, .smallfont, select, .postbit_box, label, .button{color: #F5F5F5 !important}";
+
+    var style = document.createElement("style");
+    style.type = "text/css";
+    style.appendChild(document.createTextNode(css));
+    document.head.appendChild(style);
+
+    var element = document.evaluate('/html/body/table/tbody/tr/td/table[1]/tbody/tr[1]/td/a/img', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    element.id = 'ElakiriInjectLogo';
+    document.getElementById("ElakiriInjectLogo").src = "https://raw.githubusercontent.com/dimuththarindu/UserScripts/master/Elakiri/Logo.png";
 
 }
 
@@ -79,9 +94,10 @@ function funRemoveElement(pathValue) {
     element.parentNode.removeChild(element);
 }
 
+/*
 function funRemoveStyle(pathValue) {
     let element;
     element = document.evaluate(pathValue, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
     element.removeAttribute("style");
 }
-
+*/
