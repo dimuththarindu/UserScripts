c18132efcd32ee5bb230e3564a094979e7f2ccc2
DT <25923940+dimuththarindu@users.noreply.github.com>
2019-09-08 10:24:59 +0530

    Used Git revert

--- Elakiri/Project/Elakiri.user.js
+++ Elakiri/Project/Elakiri.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Elakiri
 // @namespace    UserScripts
-// @version      13.1
+// @version      14.0
 // @author       DT
 // @description  Custom Elakiri Design
 // @source       https://github.com/dimuththarindu/UserScripts
