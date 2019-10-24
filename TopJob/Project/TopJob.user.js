0b94eb3dafd45711da7955bee104fda3c5cdb9ba
DT <25923940+dimuththarindu@users.noreply.github.com>
2019-10-24 18:41:23 +0530

    Topjob v11.0

--- TopJob/Project/TopJob.user.js
+++ TopJob/Project/TopJob.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TopJob
 // @namespace    UserScripts
-// @version      10.0
+// @version      11.0
 // @author       DT
 // @description  TopJob Website User Experience
 // @source       https://github.com/dimuththarindu/UserScripts
@@ -30,8 +30,8 @@
 
 	// Remove unwanted elements
 	funRemoveElements();
-	
-	funChangeElements();	
+
+	funChangeElements();
 })();
 
 function funRemoveElements()
@@ -43,27 +43,35 @@ function funChangeElements()
 {
 	// Re-enable right click menu
 	funReenableRightClick();
-	
+
 	// Fix URLs
 	funReplaceCrazyURLs();
-	
+
 	// Fix image size
 	if(window.location.pathname.includes("/employer/JobAdvertismentServlet"))
 	{
 		funGetElement('/html/body/div/div', 'foreignDOMStyle');
 		document.getElementById("foreignDOMStyle").style.width = "90%";
-		
+
 		funGetElement('//div[@id="remark"]/p/img', 'foreignDOMImgStyle');
-		document.getElementById("foreignDOMImgStyle").style.maxWidth = "100%";	
+		document.getElementById("foreignDOMImgStyle").style.maxWidth = "100%";
+        document.getElementById("foreignDOMImgStyle").style.height = "auto";
 	}
 	else if(window.location.pathname.includes("/employer/advertismentpreview.jsp"))
 	{
 		funGetElement('//div[@id="remark"]/p/img', 'foreignDOMImgStyle');
 		document.getElementById("foreignDOMImgStyle").style.maxWidth = "100%";
-		
+        document.getElementById("foreignDOMImgStyle").style.height = "auto";
+
 		funGetElement('//div[@id="upper"]', 'foreignDOMRemPad');
 		document.getElementById("foreignDOMRemPad").style.padding = "2px";
-	}	
+	}
+    else if(window.location.pathname.includes("/vacancy"))
+	{
+		funGetElement('//div[@id="remark"]/p/a/img', 'foreignDOMImgStyle');
+		document.getElementById("foreignDOMImgStyle").style.maxWidth = "100%";
+        document.getElementById("foreignDOMImgStyle").style.height = "auto";
+	}
 }
 
 // Re-enable right click
@@ -80,7 +88,7 @@ function funGetElement(xPath, idName)
 	if (ele != null){
 		ele.id = idName;
 	}
-	
+
 	return ele;
 }
 
@@ -90,13 +98,13 @@ function funReplaceCrazyURLs()
 	// replace all URLs
     var links = document.links;
     for (var i = 0; i < links.length; i++) {
-		
+
 		if(links[i].href.includes("javascript:openSizeWindow('../employer/JobAdvertismentServlet"))
 		{
 			// Href URL: javascript:openSizeWindow('../employer/JobAdvertismentServlet?rid=3&ac=0000000419
 			// &jc=0000689612&ec=0000000552&pg=applicant/vacancybyfunctionalarea.jsp',1098,631,
 			// 'quickvacancysearch_17DH+5vzluofT1dbd2GGz7-Nf')
-			
+
 			links[i].href = links[i].href.substring(0, links[i].href.indexOf(".jsp'") + 4);
 			links[i].href = links[i].href.replace("javascript:openSizeWindow('..", window.location.origin);
 			links[i].target = "_blank";
@@ -105,7 +113,7 @@ function funReplaceCrazyURLs()
 		{
 			// Href URL: javascript:openSizeWindow('employer/JobAdvertismentServlet?ac=DEFZZZ&ec=DEFZZZ&jc=0000689952&pg=index.jsp',1098,631,'tjadv')
 			// Generated URL: http://topjobs.lk/employer/JobAdvertismentServlet?ac=DEFZZZ&ec=DEFZZZ&jc=0000689952&pg=index.jsp
-			
+
 			links[i].href = links[i].href.substring(0, links[i].href.indexOf(".jsp'") + 4);
 			links[i].href = links[i].href.replace("javascript:openSizeWindow('", window.location.origin + "/");
 			links[i].target = "_blank";
