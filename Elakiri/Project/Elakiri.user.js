a47479a0bec5b4ed561e6890b786e69c228b35e4
DT <25923940+dimuththarindu@users.noreply.github.com>
2020-05-23 21:57:50 +0530

    Removed all custom styles

--- Elakiri/Project/Elakiri.user.js
+++ Elakiri/Project/Elakiri.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Elakiri
 // @namespace    UserScripts
-// @version      20.02
+// @version      20.03
 // @author       DT
 // @description  Custom Elakiri Design
 // @source       https://github.com/dimuththarindu/UserScripts
@@ -14,7 +14,7 @@
 // @include      *://*.elakiri.lk/*
 // @include      *://elakiri.com/*
 // @include      *://elakiri.lk/*
-// @run-at       document-end
+// @run-at       document-start
 // @grant        GM_addStyle
 // @license      Apache License 2.0
 // ==/UserScript==
@@ -24,34 +24,31 @@
 funMain();
 
 function funMain() {
-	try
+	/* try
 	{
 		funNewDesign();
 	}
 	catch(err)
 	{
-		console.log("Error: " + err);
-	}
+		console.log("Elakiri UserScript Error: " + err);
+	} */
+	funNewDesign();
 }
 
 function funNewDesign() {
-
-	funChangeHoverColor();
-
-	replaceColor('#292e39', '#050505'); // Background
-	replaceColor('#313742', '#212020'); // Body
-	replaceColor('#3f454f', '#212020'); // Footer
-
-	replaceColor('#373c46', '#141414'); // QuoteHead
-	replaceColor('#3b414b', '#1B1B1B'); // QuoteBody
-	replaceColor('#4d5460', '#141414'); // QuoteBorder
-	replaceColor('#0288d1', '#0984e3'); // QuoteLine
-
-    //replaceColor('#0984e3', '#212020'); // Notice E.g.: Your account is currently awaiting approval by an admin...
-    replaceColor('#0288d1', '#212020'); // Main Heading
-    replaceColor('#029ff4', '#050505'); // Search Bar
-
 	funNavBarStyle();
+	
+	//funChangeHoverColor();
+	// replaceColor('#292e39', '#050505'); // Background
+	// replaceColor('#313742', '#212020'); // Body
+	// replaceColor('#3f454f', '#212020'); // Footer
+	// replaceColor('#373c46', '#141414'); // QuoteHead
+	// replaceColor('#3b414b', '#1B1B1B'); // QuoteBody
+	// replaceColor('#4d5460', '#141414'); // QuoteBorder
+	// replaceColor('#0288d1', '#0984e3'); // QuoteLine
+    //// replaceColor('#0984e3', '#212020'); // Notice E.g.: Your account is currently awaiting approval by an admin...
+    // replaceColor('#0288d1', '#212020'); // Main Heading
+    // replaceColor('#029ff4', '#050505'); // Search Bar
 }
 
 function funChangeHoverColor() {
@@ -64,38 +61,15 @@ function funChangeHoverColor() {
 	} else {
 		style.appendChild(document.createTextNode(css));
 	}
-
 	document.getElementsByTagName('head')[0].appendChild(style);
 }
 
 function funNavBarStyle() {
-
-	if (window.location.pathname.includes("/threads/"))
-	{
-		var navBarLeft = document.evaluate('/html/body/div[2]/div/div[3]/div[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-
-		var stlogo = document.evaluate('/html/body/div[2]/div/div[1]/div[2]/nav/div/div[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-
-		var txtbody = document.evaluate('/html/body/div[2]/div/div[3]/div[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-
-		navBarLeft.id = "navBarLeft";
-		stlogo.id = "stlogo";
-		txtbody.id = "txtbody";
-
-		$('#navBarLeft').fadeOut();
-
-		$(function() {
-		$('#stlogo').hover(function() {
-			$('#navBarLeft').fadeIn();
-		});
-		});
-
-		$(function() {
-		$('#txtbody').hover(function() {
-			$('#navBarLeft').fadeOut();
-		});
-		});
-	}
+	
+	window.addEventListener("DOMContentLoaded", function(){
+		document.getElementById('js-XFUniqueId3').click();
+		document.getElementById('uix_sidebarNav--trigger').click();
+	});
 }
 
 function replaceColor(col1, col2) {
@@ -111,29 +85,3 @@ function replaceColor(col1, col2) {
         });
     });
 }
-
-/* function fadeOut(element) {
-    var op = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op <= 0.1){
-            clearInterval(timer);
-            element.style.display = 'none';
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
-    }, 50);
-}
-
-function fadeIn(element) {
-    var op = 0.1;  // initial opacity
-    element.style.display = 'block';
-    var timer = setInterval(function () {
-        if (op >= 1){
-            clearInterval(timer);
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.1;
-    }, 10);
-} */
